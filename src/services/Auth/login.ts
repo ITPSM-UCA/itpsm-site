9858d2a66f30d38c12dcb1fa4f8b27b060b4e9b9
Walter Ayala <00189617@uca.edu.sv>
2022-05-03 20:12:13 -0600

    [ADDED] Changes in instaces

--- src/services/Auth/login.ts
+++ src/services/Auth/login.ts
@@ -1,4 +1,4 @@
-import AxiosITPSMInstance from 'instance/AxiosITPSMInstance'
+import AxiosITPSMInstance from 'instances/AxiosITPSMInstance'
 
 interface LoginRequest {
   email: string,
@@ -7,7 +7,7 @@ interface LoginRequest {
 
 const login = async (data: LoginRequest) => {
   try {
-    const response = await AxiosITPSMInstance.post('/api/login', data)
+    const response = await AxiosITPSMInstance.post('/login', data)
     return response.data.data
   } catch (error: any) {
     return { error: error?.response?.errors?.title ?? 'Su correo electrónico o contraseña es incorrecto. Revise e intente nuevamente.' }
