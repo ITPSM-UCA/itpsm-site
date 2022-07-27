4a4979762bdb9035511fc8df7401053960628b5b
alvarogarcia1010 <00111317@uca.edu.sv>
2022-07-27 17:37:13 -0600

    [ADDED] Is approved Curriculum Subject

--- src/components/Curricula/SubjectsByCurricula.tsx
+++ src/components/Curricula/SubjectsByCurricula.tsx
@@ -12,13 +12,17 @@ const SubjectsByCurricula = ({ data }:Props) => {
   return (
     <fieldset className="border border-gray-300 rounded-lg mt-4 p-4">
       <legend className="font-medium text-indigo-600">Materias asociadas</legend>
-      <SubjectForm data={{}} />
+      {!data.is_approved && (
+        <SubjectForm data={{}} />
+      )}
+
       {subjectsByCycles.map((cycle:any) => (
         <div>
           <p className="text-lg font-bold my-2">{`Ciclo ${cycle.cycle}`}</p>
           <Subjects
             subjects={cycle.subjects}
             deleteCurriculumSubject={removeCurriculumSubject}
+            curriculumIsApproved={data.is_approved}
           />
         </div>
       ))}
@@ -26,7 +30,7 @@ const SubjectsByCurricula = ({ data }:Props) => {
   )
 }
 
-const Subjects = ({ subjects, deleteCurriculumSubject }:any) => (
+const Subjects = ({ subjects, deleteCurriculumSubject, curriculumIsApproved }:any) => (
   <div className="grid grid-cols-5 gap-4">
     {subjects.map((subject:any) => (
       <div className="w-full border rounded-lg p-2">
@@ -35,15 +39,17 @@ const Subjects = ({ subjects, deleteCurriculumSubject }:any) => (
           <p className="text-sm text-center font-bold text-indigo-600">{`${subject.uv} UV`}</p>
         </div>
         <p className="text-sm text-center font-medium h-16">{subject.subject_name}</p>
-        <div className="flex justify-end">
-          <button
-            type="button"
-            className=""
-            onClick={() => deleteCurriculumSubject(subject.id)}
-          >
-            <IoTrashOutline className="h-5 w-5 text-red-500" />
-          </button>
-        </div>
+        {!curriculumIsApproved && (
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className=""
+              onClick={() => deleteCurriculumSubject(subject.id)}
+            >
+              <IoTrashOutline className="h-5 w-5 text-red-500" />
+            </button>
+          </div>
+        )}
       </div>
     ))}
   </div>
