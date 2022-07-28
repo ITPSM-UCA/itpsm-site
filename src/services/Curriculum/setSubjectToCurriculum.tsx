1dd4c5fe5c235ebae00cbe5e05474ede43397047
Walter Ayala <00189617@uca.edu.sv>
2022-07-27 18:13:05 -0600

    [ADDED] Set subject to curriculum

---
+++ src/services/Curriculum/setSubjectToCurriculum.tsx
@@ -0,0 +1,21 @@
+import apiInstance from 'instances/apiInstance'
+import { onErrorHandler } from 'utils/alerts'
+
+const setSubjectToCurriculum = async (data: any) => {
+  let response
+  try {
+    const url = `${apiInstance.defaults.baseURL}/curriculum-subjects`
+
+    response = await apiInstance.post(url, data)
+
+    response = response.data
+
+    return response.data
+  } catch (error: any) {
+    response = error.response.data
+    onErrorHandler(error.response)
+    return response
+  }
+}
+
+export default setSubjectToCurriculum
