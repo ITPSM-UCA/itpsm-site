95cc1896d5ceda6ca1c8055546a0bb8c2cfef2d6
Slackoth <00082318@uca.edu.sv>
2023-01-21 22:01:16 -0600

    [ADDED] Archivo para interfaces genericas que pueden ser utilizadas en diferentes componentes

---
+++ src/interfaces/interface.d.ts
@@ -0,0 +1,15 @@
+export {}
+
+declare global {
+    interface TableProps {
+        columns: any[],
+        tableRef: any,
+        // Data
+        fetchData: (query: any) => Promise<any>
+        // States
+        toggleForm: () => void,
+        // Actions
+        editRowAction: any,
+        refreshTableAction: () => void,
+    }
+}
