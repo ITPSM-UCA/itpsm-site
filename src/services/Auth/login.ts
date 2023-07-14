1163fa6dd9de16921a04a4b77f8d3289cae19be6
renemel <rene.melendeza@gmail.com>
2023-07-14 16:14:37 -0600

    [add] header on post

--- src/services/Auth/login.ts
+++ src/services/Auth/login.ts
@@ -9,7 +9,11 @@ const login = async (data: LoginRequest) => {
   let response
   try {
     const url = `${apiInstance.defaults.baseURL}/login`
-    response = await apiInstance.post(url, data)
+    response = await apiInstance.post(url, data,{
+      headers:{
+        'Content-Type': 'application/json',
+      }
+    })
     return response.data.data
   } catch (error: any) {
     return { error: error?.response?.errors?.title ?? 'Su correo electrónico o contraseña es incorrecto. Revise e intente nuevamente.' }
